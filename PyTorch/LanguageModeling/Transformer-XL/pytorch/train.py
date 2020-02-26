263cd29a66857531e85fdbc1518f009779b686c6
Ahmed Elnaggar <amit.najjar@gmail.com>
2020-02-27 00:24:43 +0100

    fix iterator

--- PyTorch/LanguageModeling/Transformer-XL/pytorch/train.py
+++ PyTorch/LanguageModeling/Transformer-XL/pytorch/train.py
@@ -396,7 +396,7 @@ def train(tr_iter, va_iter, model, para_model, model_config, optimizer,
 
     mems = [None for _ in range(args.batch_chunk)]
     #train_iter = tr_iter.get_varlen_iter() if args.varlen else tr_iter
-    train_iter = tr_iter
+    train_iter = tr_iter.get_varlen_iter() if args.varlen else get_fixlen_iter
 
     for batch, (data, target, seq_len, _) in enumerate(train_iter):
         log_step += 1
