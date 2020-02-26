611b84572b546b7af9d9a208a08b01a0669b1911
Ahmed Elnaggar <amit.najjar@gmail.com>
2020-02-26 23:53:35 +0100

    remove cache

--- PyTorch/LanguageModeling/Transformer-XL/pytorch/data_utils.py
+++ PyTorch/LanguageModeling/Transformer-XL/pytorch/data_utils.py
@@ -308,33 +308,33 @@ def get_lm_corpus(datadir, dataset, vocab):
     else:
         raise RuntimeError('Unsupported vocab')
 
-    if os.path.exists(fn):
-        logging.info('Loading cached dataset...')
-        corpus = torch.load(fn)
-    else:
-        logging.info('Producing dataset {}...'.format(dataset))
-        kwargs = {}
-        if dataset in ['wt103', 'wt2']:
-            kwargs['special'] = ['<eos>']
-            kwargs['lower_case'] = False
-        elif dataset == 'ptb':
-            kwargs['special'] = ['<eos>']
-            kwargs['lower_case'] = True
-        elif dataset == 'lm1b':
-            kwargs['special'] = []
-            kwargs['lower_case'] = False
-            kwargs['vocab_file'] = os.path.join(datadir, '1b_word_vocab.txt')
-        elif dataset in ['bfd']:
-            kwargs['special'] = []
-            kwargs['lower_case'] = False
-            kwargs['vocab_file'] = os.path.join(datadir, 'vocab.txt')
-        elif dataset in ['enwik8', 'text8']:
-            pass
+    #if os.path.exists(fn):
+    #    logging.info('Loading cached dataset...')
+    #    corpus = torch.load(fn)
+    #else:
+    logging.info('Producing dataset {}...'.format(dataset))
+    kwargs = {}
+    if dataset in ['wt103', 'wt2']:
+        kwargs['special'] = ['<eos>']
+        kwargs['lower_case'] = False
+    elif dataset == 'ptb':
+        kwargs['special'] = ['<eos>']
+        kwargs['lower_case'] = True
+    elif dataset == 'lm1b':
+        kwargs['special'] = []
+        kwargs['lower_case'] = False
+        kwargs['vocab_file'] = os.path.join(datadir, '1b_word_vocab.txt')
+    elif dataset in ['bfd']:
+        kwargs['special'] = []
+        kwargs['lower_case'] = False
+        kwargs['vocab_file'] = os.path.join(datadir, 'vocab.txt')
+    elif dataset in ['enwik8', 'text8']:
+        pass
 
-        corpus = Corpus(datadir, dataset, vocab, **kwargs)
-        with utils.distributed.sync_workers() as rank:
-            if rank == 0:
-                torch.save(corpus, fn)
+    corpus = Corpus(datadir, dataset, vocab, **kwargs)
+    with utils.distributed.sync_workers() as rank:
+        if rank == 0:
+            torch.save(corpus, fn)
 
     return corpus
 
