6b6eaec93934c6a3b8275a6c239223a94e64d310
Ahmed Elnaggar <amit.najjar@gmail.com>
2020-02-26 23:41:20 +0100

    change valid iterator

--- PyTorch/LanguageModeling/Transformer-XL/pytorch/data_utils.py
+++ PyTorch/LanguageModeling/Transformer-XL/pytorch/data_utils.py
@@ -295,7 +295,7 @@ class Corpus(object):
             elif self.dataset == 'lm1b':
                 data_iter = LMShuffledIterator(data, *args, **kwargs)
             elif self.dataset == 'bfd':
-                data_iter = LMOrderedIterator(data, *args, **kwargs)
+                data_iter = LMOrderedIterator(self.valid, *args, **kwargs)
 
         return data_iter
 
